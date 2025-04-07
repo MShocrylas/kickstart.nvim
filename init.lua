6dd61c3858aeb00babc6d0f0951a798b73a5d02d
Matt Shocrylas <matthew.shocrylas@siemens.com>
2025-04-07 15:45:48 -0600

    Add python formatter

--- init.lua
+++ init.lua
@@ -712,6 +712,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         cpp = { 'clang-format' },
+        python = { 'black' },
 
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
